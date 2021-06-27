ffdecf2c9b81d4a3a00c29cdf136db8ea1c72065
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-06-27 22:28:58 +0100

    Adding drawer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import Tabs from '@material-ui/core/Tabs';
+import { useState } from 'react';
 import Tab from '@material-ui/core/Tab';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -14,11 +15,21 @@ import Badge from '@material-ui/core/Badge';
 import { makeStyles } from '@material-ui/styles';
 import Hidden from '@material-ui/core/Hidden';
 import Box from '@material-ui/core/Box';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { useTheme } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
+const drawerWidth = '100%';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-
+    display: 'flex',
   },
   toolbarWrapper: {
     width: '900px',
@@ -44,53 +55,117 @@ const useStyles = makeStyles((theme) => ({
   icons: {
     fontSize: '29px',
     marginLeft: '13px'
-  }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
 }));
 
 export default function Navbar({ props }) {
   const classes = useStyles();
 
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <AppBar className={classes.root}>
-      <Toolbar className={classes.toolbarWrapper}>
-        <Hidden mdUp>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-        </Hidden>
+    <Box className={classes.root}>
+      <AppBar>
+        <Toolbar className={classes.toolbarWrapper}>
+          <Hidden mdUp>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
 
-        <Typography variant="h7">JOBPLUS</Typography>
+          <Typography variant="h7">JOBPLUS</Typography>
 
-        <Hidden smDown>
-          <Tabs>
-            <Tab className={classes.tabs} label="Home" />
-            <Tab className={classes.tabs} label="Job listing" />
-            <Tab className={classes.tabs} label="Job applications" />
-          </Tabs>
-        </Hidden>
+          <Hidden smDown>
+            <Tabs>
+              <Tab className={classes.tabs} label="Home" />
+              <Tab className={classes.tabs} label="Job listing" />
+              <Tab className={classes.tabs} label="Job applications" />
+            </Tabs>
+          </Hidden>
 
-        <Box className={classes.iconsWrap}>
-          <IconButton edge="start" color="inherit">
-            <SearchIcon className={classes.icons} />
+          <Box className={classes.iconsWrap}>
+            <IconButton edge="start" color="inherit">
+              <SearchIcon className={classes.icons} />
+            </IconButton>
+            <IconButton edge="start" color="inherit">
+              <Badge color="error" overlap="circle" variant="dot">
+                <NotificationsNoneIcon className={classes.icons} />
+              </Badge>
+            </IconButton>
+            <IconButton edge="start" color="inherit" >
+              <Badge badgeContent={2} classes={{ badge: classes.badge }}>
+                <StarBorderIcon className={classes.icons} />
+              </Badge>
+            </IconButton>
+            <IconButton edge="start" color="inherit">
+              <PersonOutlineIcon className={classes.icons} />
+            </IconButton>
+            <IconButton edge="start" color="inherit">
+              <ExitToAppIcon className={classes.icons} />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
-          <IconButton edge="start" color="inherit">
-            <Badge color="error" overlap="circle" variant="dot">
-              <NotificationsNoneIcon className={classes.icons} />
-            </Badge>
-          </IconButton>
-          <IconButton edge="start" color="inherit" >
-            <Badge badgeContent={2} classes={{ badge: classes.badge }}>
-              <StarBorderIcon className={classes.icons} />
-            </Badge>
-          </IconButton>
-          <IconButton edge="start" color="inherit">
-            <PersonOutlineIcon className={classes.icons} />
-          </IconButton>
-          <IconButton edge="start" color="inherit">
-            <ExitToAppIcon className={classes.icons} />
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemText primary={'HOME'} />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary={'JOB LISTING'} />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary={'JOB APPLICATION'} />
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
   )
 }
